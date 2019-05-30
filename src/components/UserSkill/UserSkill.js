7b36eb5067b4f5d0f9d6f25f3613b9d7e1706518
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-05-30 14:53:05 +0200

    Add user detail content

---
+++ src/components/UserSkill/UserSkill.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.css';
+
+const UserSkill = ({
+  numberOfVotes,
+  skill,
+  voteStatus
+}) => (
+  <div className="user-skill-container">
+    <div className="vote-number">{numberOfVotes}</div>
+    <div className="skill">{skill}</div>
+    <div className="vote-button">
+      <img src={voteStatus}/>
+    </div>
+  </div>
+);
+
+export default UserSkill;
--- src/screens/UserProfileDetail/UserProfileDetail.js
+++ src/screens/UserProfileDetail/UserProfileDetail.js
@@ -4,7 +4,14 @@ import './style.css';
 
 import IsMobileSize from '../../helpers/MobileDetect';
 import UserAvatar from '../../components/UserAvatar/UserAvatar';
+import UserSkill from '../../components/UserSkill/UserSkill';
+
 import userAvatarWomen from "../../assets/img_avatar_women.png";
+import iconCall from '../../assets/iconCall.png';
+import iconChat from '../../assets/iconChat.png';
+import showMoreIcon from '../../assets/showMoreArrow.png';
+import votedIcon from '../../assets/voted.png';
+import notVotedIcon from '../../assets/notVoted.png';
 
 class UserProfileDetail extends Component {
   constructor(props, context) {
@@ -12,15 +19,55 @@ class UserProfileDetail extends Component {
 
     this.state = {
       isOnMobileSize: IsMobileSize(),
+      showText: false,
       user: {
         userProfileImage: userAvatarWomen,
         userActiveStatus: 'active',
         userName: 'Doc Emilia',
+        profession: 'postdoc',
         userDescription:
-          'I’m the marketing manager and co-founder of Sisters in Business and'
-      }
+          'I\'m RSE Enterprise Fellow in the Biochemistry Department in Cambridge and a GFC Fellow in Innovation & Entrepreneurship at the World'
+      },
+      userSkills: [
+        {
+          numberOfVotes: '174',
+          skill: 'BioChemistry',
+          voteStatus: votedIcon
+        },
+        {
+          numberOfVotes: '154',
+          skill: 'Biofuels',
+          voteStatus: notVotedIcon
+        },
+        {
+          numberOfVotes: '174',
+          skill: 'Industrial-academic',
+          voteStatus: notVotedIcon
+        },
+        {
+          numberOfVotes: '174',
+          skill: 'Science communication',
+          voteStatus: votedIcon
+        },
+        {
+          numberOfVotes: '174',
+          skill: 'Entrepreneurship',
+          voteStatus: notVotedIcon
+        },
+        {
+          numberOfVotes: '174',
+          skill: 'Industrial-academic',
+          voteStatus:  notVotedIcon
+        },
+      ]
     }
   }
+
+  componentDidMount() {
+    this.windowResize();
+    window.addEventListener('resize', this.windowResize);
+  }
+
   componentWillUnmount() {
     window.removeEventListener('resize', this.windowResize);
   }
@@ -30,8 +77,24 @@ class UserProfileDetail extends Component {
     console.log(IsMobileSize());
   };
 
+  handleShowMoreContentButtonClick = () => {
+    const { user, showText } = this.state;
+
+    if(showText) {
+      
+    }
+
+    this.setState(
+      {
+          showText: user.userDescription.length < 130
+          ? !showText
+          : showText
+      });
+    console.log(showText);
+  };
+
   render() {
-    const { isOnMobileSize, user } = this.state;
+    const { isOnMobileSize, user, userSkills } = this.state;
     // const {
     //   userProfileImage,
     //   userActiveStatus
@@ -50,6 +113,42 @@ class UserProfileDetail extends Component {
             />
           </div>
           <div className="user-detail-name">{user.userName}</div>
+        </div>
+        <div className="contact-section">
+          <div className="icon-contact icon-call">
+            <img src={iconCall}/>
+          </div>
+          <div className="icon-contact icon-chat">
+            <img src={iconChat}/>
+          </div>
+        </div>
+        <div className="profile-content">
+          <div className="user-position-container">
+            <div className="user-position">
+              <p>University <br/> Alumni Network</p>
+            </div>
+          </div>
+          <div className="user-detail-description">
+            <div className="profession-tag">{user.profession}</div>
+            <p className="description">
+              {user.userDescription.length < 130
+              ? `${user.userDescription}`
+              : `${user.userDescription.substring(0, 135)} ...`}
+            </p>
+            <div className="show-more-description" onClick={this.handleShowMoreContentButtonClick}>
+              <p>show more</p>
+              <img src={showMoreIcon}/>
+            </div>
+          </div>
+          <div className="skills-container">
+            {userSkills.map((skill, id) => (
+              <UserSkill
+                key={id}
+                numberOfVotes={skill.numberOfVotes}
+                skill={skill.skill}
+                voteStatus={skill.voteStatus}
+            />))}
+          </div>
         </div>
       </div>
     ) : (
