6bcae4a3f97a9b3bad3a4a60aa2dd2b24f8ef5a6
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-06 14:30:24 +0300

    Build commit

---
+++ build/precache-manifest.9a25cf432b72a6dafce98ac89d23285e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bd561046dd67c5668325665c35caa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c24efbb4b6de4e1915a",
+    "url": "/static/css/main.e0564d54.chunk.css"
+  },
+  {
+    "revision": "ac57131a74328947934c",
+    "url": "/static/js/2.4a658650.chunk.js"
+  },
+  {
+    "revision": "6c24efbb4b6de4e1915a",
+    "url": "/static/js/main.7fcbde00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a129e1a4c09eb302b17606b9c5121ac6",
+    "url": "/static/media/doc_emilia_avatar.a129e1a4.png"
+  },
+  {
+    "revision": "9234764f3c2c30f5b615b62f46e688c1",
+    "url": "/static/media/group3.9234764f.png"
+  },
+  {
+    "revision": "fd60bbf74b653201d638b7393a4b1214",
+    "url": "/static/media/iconCall.fd60bbf7.png"
+  },
+  {
+    "revision": "df4cb4d9028a741b985767f051091082",
+    "url": "/static/media/iconChat.df4cb4d9.png"
+  }
+]);
--- src/screens/OrganizationScreen/OrganizationScreen.js
+++ src/screens/OrganizationScreen/OrganizationScreen.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 import './style.css';
 
+import ScreenHeader from '../../components/ScreenHeader/ScreenHeader';
+
 class OrganizationScreen extends Component {
   render() {
     return(
       <div className="organization-container">
         <div className="organization-header">
+          <ScreenHeader screenHeaderName="CHANNEL XYZ"/>
           <div>
-
+            hello
           </div>
         </div>
       </div>
