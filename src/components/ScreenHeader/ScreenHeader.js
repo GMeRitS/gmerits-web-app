02eb1b171d867fb73eacf0485122723e7182027b
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-07 16:30:32 +0300

    Change back button image to icon and styles

--- src/components/ScreenHeader/ScreenHeader.js
+++ src/components/ScreenHeader/ScreenHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 import './style.css';
 import backIcon from '../../assets/back_icon.png';
@@ -14,7 +15,8 @@ const ScreenHeader = ({ headerBackgroundColor, screenHeaderName, routePushBack, 
         className="back-button"
         onClick={routePushBack}
       >
-        <img src={backIcon} alt="" />
+        {/*<img src={backIcon} alt="" />*/}
+        <FontAwesomeIcon className="icon-back" icon={faChevronLeft} />
       </button>
       <div className="screen-header-name">{screenHeaderName}</div>
       <button className="favourite-button" onClick={favouriteCheck}>
