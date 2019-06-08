0b5d55f849b31e2e4fd52fdb06ca68a8c557bf86
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-08 23:05:14 +0300

    Complete setting screen

--- src/components/SettingsItem/SettingsItem.js
+++ src/components/SettingsItem/SettingsItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Switch from 'react-switch';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 import './style.css';
 
@@ -9,7 +9,7 @@ class SettingsItem extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { checked: false };
+    this.state = { checked: true };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -18,26 +18,49 @@ class SettingsItem extends Component {
   }
 
   render() {
-    const { settingsName, buttonVisible, arrowVisible } = this.props;
+    const {
+      settingName,
+      buttonVisible,
+      arrowVisible,
+      logoutIconVisible,
+      switchActiveText,
+      switchInactiveText
+    } = this.props;
+    const { checked } = this.state;
 
     return (
       <div className="settings-item-container">
         <div className="settings-item-sub-container">
           <div className="setting-item-name">
-            <p>Status</p>
+            <p>{settingName}</p>
           </div>
-          {buttonVisible && <div className="switch-button">
-            <label>
-              <span>Online</span>
-              <Switch onChange={this.handleChange} checked={this.state.checked} />
-            </label>
-          </div>}
-          {arrowVisible && <div className="arrow-right">
-            <FontAwesomeIcon className="icon-arrow-right" icon={faChevronRight} />
-          </div>}
+          {buttonVisible && (
+            <div className="switch-button">
+              <label>
+                <span>{checked ? switchActiveText : switchInactiveText}</span>
+                <Switch
+                  onChange={this.handleChange}
+                  checked={this.state.checked}
+                />
+              </label>
+            </div>
+          )}
+          {arrowVisible && (
+            <div className="arrow-right">
+              <FontAwesomeIcon
+                className="icon-arrow-right"
+                icon={faChevronRight}
+              />
+            </div>
+          )}
+          {logoutIconVisible && (
+            <div>
+              <FontAwesomeIcon className="logout-setting-button" icon={faSignOutAlt} />
+            </div>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -29,12 +29,118 @@ class SettingsScreen extends Component {
     const { isOnMobileSize } = this.state;
     return isOnMobileSize ? (
       <div className="setting-screen-container">
-        <div className="setting-screen-header">
+        <div className="settings-screen-header">
           <p>SETTINGS</p>
         </div>
-        <div className="setting-screen-content">
-          <SettingsItem buttonVisible={true} arrowVisible={false} />
-          <SettingsItem buttonVisible={false} arrowVisible={true} />
+        <div className="settings-screen-content">
+          <div className="setting">
+            <p>USER ACCOUNT</p>
+          </div>
+          <SettingsItem
+            settingName="Status"
+            switchActiveText="Online"
+            switchInactiveText="Offline"
+            buttonVisible={true}
+            arrowVisible={false}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Edit profile"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Your Organizations"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <div className="setting">
+            <p>PRIVACY AND SECURITY</p>
+          </div>
+          <SettingsItem
+            settingName="Who can contact me?"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Profile visibility"
+            switchActiveText="Visible"
+            switchInactiveText="Invisible"
+            buttonVisible={true}
+            arrowVisible={false}
+            logoutIconVisible={false}
+          />
+          <div className="setting-note">
+            <p>
+              If you hide your profile the other users will not find it in the
+              search results. However, all the information stored on your
+              profile is stored safely. You can change your profile back to
+              visible at anytime.
+            </p>
+          </div>
+          <div className="setting">
+            <p>NOTIFICATIONS</p>
+          </div>
+          <SettingsItem
+            settingName="Push notifications"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Email and SMS notifications"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <div className="setting">
+            <p>SUPPORT</p>
+          </div>
+          <SettingsItem
+            settingName="Send Feedback"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Switch language"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <div className="setting">
+            <p>ABOUT</p>
+          </div>
+          <SettingsItem
+            settingName="Service Terms"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="Privacy Policy"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <SettingsItem
+            settingName="About [App Name]"
+            buttonVisible={false}
+            arrowVisible={true}
+            logoutIconVisible={false}
+          />
+          <div className="setting">
+            <p>LOGINS</p>
+          </div>
+          <SettingsItem
+            settingName="Log out"
+            buttonVisible={false}
+            arrowVisible={false}
+            logoutIconVisible={true}
+          />
         </div>
       </div>
     ) : (
