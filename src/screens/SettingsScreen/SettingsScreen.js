a0ba03a412d1792634a01f65b12d4002e8e2a015
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-11 08:06:23 +0300

    Delete unused component

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -5,8 +5,7 @@ import history from '../../history';
 import IsMobileSize from '../../helpers/MobileDetect';
 import SettingsItem from '../../components/SettingsItem/SettingsItem';
 import ScreenHeader from '../../components/ScreenHeader/ScreenHeader';
-import RoutePathConstants from '../../constants/RoutePathConstants';
-import SideMenu from '../../components/SideMenu/SideMenu';
+import RoutePathConstants from '../../constants/RoutePathConstants';  
 
 const { editProfile } = RoutePathConstants;
 
