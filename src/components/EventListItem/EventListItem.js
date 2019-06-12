0fddd7e697ebfa95c08c7bb61b976491aa286304
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-12 22:09:35 +0300

    Complete event list view

--- src/components/EventListItem/EventListItem.js
+++ src/components/EventListItem/EventListItem.js
@@ -3,15 +3,20 @@ import React from 'react';
 import './style.css';
 
 const EventListItem = ({
-
+  eventMonth,
+  eventDay,
+  eventName
 }) => {
   return(
     <div className="event-list-item-container">
       <div className="event-date-container">
-        <div className="event-date">JUNE 12</div>
+        <div className="event-date">
+          <p className="event-month">{eventMonth}</p>
+          <p className="event-day">{eventDay}</p>
+        </div>
       </div>
       <div className="event-name">
-        <p>Managing People for the First Time</p>
+        <p>{eventName}</p>
       </div>
     </div>
   )
--- src/screens/EventsListScreen/EventsListScreen.js
+++ src/screens/EventsListScreen/EventsListScreen.js
@@ -12,6 +12,44 @@ class EventsListScreen extends Component {
 
     this.state = {
       isOnMobileSize: IsMobileSize(),
+      events: [
+        {
+          id: 1,
+          eventName: 'Managing People for the First Time',
+          eventMonth: 'Jun',
+          eventDay: '12'
+        },
+        {
+          id: 2,
+          eventName: 'CW Talk: Solving 5G rural connectivity: regulation or innovation?',
+          eventMonth: 'Jun',
+          eventDay: '12'
+        },
+        {
+          id: 3,
+          eventName: 'Our experience: how to apply compliant data management?',
+          eventMonth: 'Jun',
+          eventDay: '13'
+        },
+        {
+          id: 4,
+          eventName: 'Presentation Skills: Advanced',
+          eventMonth: 'Jun',
+          eventDay: '14'
+        },
+        {
+          id: 5,
+          eventName: 'Reactive, Predictive, Prescriptive – Evolving your IIoT',
+          eventMonth: 'Jun',
+          eventDay: '14'
+        },
+        {
+          id: 6,
+          eventName: 'Soft Skills for Tech Leaders Who Think Coaching is a Waste of Time?',
+          eventMonth: 'Jun',
+          eventDay: '15'
+        }
+      ]
     }
   }
 
@@ -32,7 +70,7 @@ class EventsListScreen extends Component {
 
 
   render() {
-    const { isOnMobileSize } = this.state;
+    const { isOnMobileSize, events } = this.state;
     return isOnMobileSize ? (
       <div className="event-list-container">
         <ScreenHeader
@@ -40,7 +78,14 @@ class EventsListScreen extends Component {
           closeIconVisible={true}
         />
         <div className="event-list">
-          <EventListItem/>
+          {events.map((event, id) => (
+            <EventListItem
+              key={id}
+              eventName={event.eventName}
+              eventDay={event.eventDay}
+              eventMonth={event.eventMonth}
+            />
+          ))}
         </div>
       </div>
     ) : (
