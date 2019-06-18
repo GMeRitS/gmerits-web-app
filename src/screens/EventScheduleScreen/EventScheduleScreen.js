c2cb48373125c6a788c33d2b08d59df94da51550
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-06-18 10:44:51 +0300

    Implement schedule content sample

--- src/screens/EventScheduleScreen/EventScheduleScreen.js
+++ src/screens/EventScheduleScreen/EventScheduleScreen.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 import './style.css';
+import history from '../../history';
+
 import IsMobileSize from '../../helpers/MobileDetect';
 import events from '../../MockData/Events';
+
+import RoutePathConstants from '../../constants/RoutePathConstants';
+
+const { eventDetail } = RoutePathConstants;
 
 class EventScheduleScreen extends Component {
   constructor(props, context) {
@@ -30,12 +36,22 @@ class EventScheduleScreen extends Component {
     this.setState({ isOnMobileSize: IsMobileSize() });
   };
 
+  handleSessionItemClick = () => {
+    history.push(`/${eventDetail}`)
+  };
+
   render() {
     const { isOnMobileSize } = this.state;
 
     return isOnMobileSize ? (
       <div className="event-schedule-container">
-        <div className="event-schedule-content">this will be a schedule</div>
+        <div className="event-schedule-content">
+          <p>This will be a schedule</p>
+          <div className="session-item" onClick={this.handleSessionItemClick}>
+            <div className="important-session-side-highlight" />
+            <div className="session-content">session example</div>
+          </div>
+        </div>
       </div>
     ) : (
       <div>Too big screen</div>
