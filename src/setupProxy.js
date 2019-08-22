3416e44a1eed915dee48b06ac574c1f99e5a1f79
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-08-22 12:30:35 +0300

    Format code

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,12 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api/media', { pathRewrite: { '^/api/media': '' }, target: 'https://testmedia.mesensei.com/', 'secure': false, 'changeOrigin': true }));
+  app.use(
+    proxy('/api/media', {
+      pathRewrite: { '^/api/media': '' },
+      target: 'https://testmedia.mesensei.com/',
+      secure: false,
+      changeOrigin: true
+    })
+  );
 };
