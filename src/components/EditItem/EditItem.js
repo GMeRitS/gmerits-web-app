559a078a90ff9bf0ac6dd9a6dbc12595eda6b7b9
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-09-11 12:51:14 +0300

    Fix edit username limit length based on screen size

--- src/components/EditItem/EditItem.js
+++ src/components/EditItem/EditItem.js
@@ -66,7 +66,7 @@ class EditItem extends Component {
               type="text"
               onChange={onUserNameInputChange}
               placeholder="Username"
-              maxLength="21"
+              maxLength={window.innerWidth <= 320 ? '16' : '21'}
             />
           )}
           {editGenderVisible && (
