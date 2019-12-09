be2f2c611c0e4abec0d88acf430a49ad0e74b4b5
Joonatan Nguyen <minhnguyen181199@gmail.com>
2019-12-09 11:53:35 +0200

    Update build

---
+++ build/precache-manifest.7530ff0f424d1fbf1e22581a03e0396f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f553e791f80b065dbd070a579c81c7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b98641eed0f90f57e4bc",
+    "url": "/static/css/2.76c98fb9.chunk.css"
+  },
+  {
+    "revision": "528ed91c703bc34251df",
+    "url": "/static/css/main.2c2d8a74.chunk.css"
+  },
+  {
+    "revision": "b98641eed0f90f57e4bc",
+    "url": "/static/js/2.f1985a38.chunk.js"
+  },
+  {
+    "revision": "528ed91c703bc34251df",
+    "url": "/static/js/main.40cde9c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35fadd4be56bb4612447f71d80a1f883",
+    "url": "/static/media/USHeaderLogo.35fadd4b.png"
+  },
+  {
+    "revision": "9234764f3c2c30f5b615b62f46e688c1",
+    "url": "/static/media/group3.9234764f.png"
+  },
+  {
+    "revision": "35d9972360c9679837e841ed335a5974",
+    "url": "/static/media/ic_chat_button.35d99723.png"
+  },
+  {
+    "revision": "124489caf8faa8fd5145a54b7202b168",
+    "url": "/static/media/iconAppNewCoHel.124489ca.png"
+  },
+  {
+    "revision": "29d9e3a05f6d1df34f385638358fa63e",
+    "url": "/static/media/iconAppShip.29d9e3a0.png"
+  },
+  {
+    "revision": "1289330093771219a386cd19ebb515c2",
+    "url": "/static/media/iconAppUnifiedSci.12893300.png"
+  },
+  {
+    "revision": "37a9f17f6dc433a9e8d84162b699dc51",
+    "url": "/static/media/iconBooked.37a9f17f.png"
+  },
+  {
+    "revision": "b308415a4b9dbcc936365603b16252c3",
+    "url": "/static/media/unifiedScienceBg.b308415a.png"
+  }
+]);
