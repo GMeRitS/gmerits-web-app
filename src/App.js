b9fd156b498bf0f93e599f511d8a741b98e3bf0c
Joonatan Nguyen <minhnguyen181199@gmail.com>
2020-01-03 12:00:05 +0200

    Update fixing validate login token called when login data is empty

--- src/App.js
+++ src/App.js
@@ -92,7 +92,10 @@ class App extends Component {
     const invitetoken = urlParams.get('invitetoken');
 
     const loginData = { logintoken: logintoken, invitetoken: invitetoken };
-    this.props.validateLoginData(!_isEmpty(loginData) ? loginData : '');
+    
+    if(!_isEmpty(loginData.logintoken) && !_isEmpty(loginData.invitetoken)) {
+      this.props.validateLoginData(loginData);
+    }
   };
 
   render() {
